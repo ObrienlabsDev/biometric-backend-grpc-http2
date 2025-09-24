c1e638d6e8f21ae994773ec852469b443a3d69d8
Michael O'Brien <michael@obrienlabs.org>
2025-09-23 22:46:42 -0400

    #1 - spring boot template

---
+++ src/test/java/dev/obrienlabs/biometric/BiometricBackendGrpcApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.obrienlabs.biometric;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiometricBackendGrpcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
